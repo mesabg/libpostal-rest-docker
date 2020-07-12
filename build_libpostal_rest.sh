59d72ccd78d09cc221779790cdc69af923d34cc7
Moisés Berenguer <moises.berenguer@gmail.com>
2020-07-12 14:58:05 -0400

    Minor public change

--- build_libpostal_rest.sh
+++ build_libpostal_rest.sh
@@ -11,6 +11,6 @@ export GOROOT=/libpostal/go
 export GOPATH=/libpostal/workspace
 export PATH=$PATH:/libpostal/go/bin
 
-go get github.com/johnlonganecker/libpostal-rest
+go get github.com/mesabg/libpostal-rest
 
-go install github.com/johnlonganecker/libpostal-rest
+go install github.com/mesabg/libpostal-rest
